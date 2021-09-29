984fd71eaae9b3d5955155595cec1583d24c02f0
Mihail Sergeev <Mihail-Ol-Sergeev@yandex.ru>
2021-09-29 18:13:45 +0300

    Сохранены изменения файла README, а также добавлен учебный файл для Python

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning-VSCode
 Learning of use VSCode
+Print in Visual Studio.
+print(f'gff')
---
+++ Функции.py
@@ -0,0 +1,8 @@
+def square(numb):
+  result = numb ** 2
+  return result
+
+print(square(7))
+
+power = lambda x, y : x ** y
+print(power(3, 4))
